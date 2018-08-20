b2b1a632c82b8859faa23e2705c2f9b52351fae2
Corinne Krych <corinnekrych@gmail.com>
2018-08-20 15:08:49 +0200

    fix(semaphore): add verify condition

---
+++ verifyConditions.js
@@ -0,0 +1,10 @@
+// /verifyConditions.js
+const SRError = require('@semantic-release/error');
+
+module.exports = function (pluginConfig, config, callback) {
+    if (config.env.SEMAPHORE === 'true') {
+        callback(null);
+    } else {
+        callback(new SRError('Not running on Semaphore, won\'t be published.'));
+    }
+}
